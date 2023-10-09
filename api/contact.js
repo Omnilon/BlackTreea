15f3fae4a876c31f2cee15f14c64bfdc8170f4ce
Omnilon <97851047+Omnilon@users.noreply.github.com>
2023-10-09 01:04:47 -0400

    contactjshtmlfull

--- api/contact.js
+++ api/contact.js
@@ -1,12 +1,21 @@
 const axios = require('axios');
 
 module.exports = async (req, res) => {
-    const { name, message } = req.body;
+    const { orderName, address, paymentMethod, paymentDetails, name, message } = req.body;
 
     const TELEGRAM_TOKEN = '6338711548:AAHAdZ3MnM7M9IMTIEkC5Y-aoq-SvJNEx_s';
     const TELEGRAM_CHAT_ID = '6683416611';
 
-    const text = `New Contact Message:\nName: ${name}\nMessage: ${message}`;
+    let text = '';
+
+    if (orderName && address && paymentMethod && paymentDetails) {
+        text = `New Order:\nName or Alias: ${orderName}\nAddress: ${address}\nPayment Method: ${paymentMethod}\nPayment Details: ${paymentDetails}`;
+    } else if (name && message) {
+        text = `New Contact Message:\nName: ${name}\nMessage: ${message}`;
+    } else {
+        res.status(400).send('Invalid form submission.');
+        return;
+    }
 
     try {
         await axios.post(`https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`, {
