8bfecaeb08db8af4ce6fa148781c68636f69eebc
Omnilon <97851047+Omnilon@users.noreply.github.com>
2023-11-07 14:38:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const treeSelection = document.getElementById('treeSelection');
-    const totalPrice = document.getElementById('totalPrice');
-    const treePrices = {
-        "Oak - $10": 10,
-        "Pine - $15": 15,
-        "Birch - $20": 20,
-        "Maple - $25": 25
-    };
-
-    treeSelection.addEventListener('change', function() {
-        let total = 0;
-        for (let option of treeSelection.selectedOptions) {
-            total += treePrices[option.value];
-        }
-        totalPrice.textContent = "Total: $" + total;
-    });
-
     fetch('/cutOffTime.json')
     .then(response => response.json())
     .then(data => {
