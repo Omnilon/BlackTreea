86e41920946d3cd26c62b0b36c69c2180db3d8bc
Omnilon <97851047+Omnilon@users.noreply.github.com>
2023-11-15 14:43:37 -0500

    Update order.js

--- api/order.js
+++ api/order.js
@@ -3,46 +3,29 @@ const cutOffTime = require('../cutOffTime.json'); // Importing the cutOffTime.js
 
 // Convert 12-hour format time to minutes since midnight
 function timeToMinutes(timeStr) {
-    const [hours, minutes] = timeStr.split(':').map(Number);
-    const isPM = timeStr.includes('PM');
-    return ((hours % 12) + (isPM ? 12 : 0)) * 60 + minutes;
+    const [time, modifier] = timeStr.split(' ');
+    let [hours, minutes] = time.split(':').map(Number);
+    if (modifier === 'PM') {
+        hours = (hours % 12) + 12;
+    }
+    return hours * 60 + minutes;
 }
 
 module.exports = async (req, res) => {
-    let { igName, phoneNumber, address, mailboxDropoff, paymentMethod, paymentDetails, treeSelection, name, message } = req.body;
+    let { igName, phoneNumber, address, mailboxDropoff, paymentMethod, paymentDetails, customRequest, name, message } = req.body;
 
-    const TELEGRAM_TOKEN = '6501676804:AAEOKcy_JfF-W7IxXZyT8k5Qvn7zyHiHptc';
-    const TELEGRAM_CHAT_ID = '1055897795';
-
-    console.log("Before conversion: ", typeof treeSelection, treeSelection);
-
-    // Ensure treeSelection is always an array
-    if (!Array.isArray(treeSelection)) {
-        treeSelection = [treeSelection];
-    }
-
-    console.log("After conversion: ", typeof treeSelection, treeSelection);
-
-    let text = '';
-    const treePrices = {
-        "Oak": 50,
-        "Pine": 40,
-        "Birch": 45,
-        "Maple": 55
-    };
+    const TELEGRAM_TOKEN = '6338711548:AAHAdZ3MnM7M9IMTIEkC5Y-aoq-SvJNEx_s';
+    const TELEGRAM_CHAT_ID = 'BlackTreea';
 
     const currentMinutes = new Date().getHours() * 60 + new Date().getMinutes();
     const cutOffMinutes = timeToMinutes(cutOffTime.time); // Using the function to get cut-off minutes
 
     let afterCutOff = currentMinutes > cutOffMinutes;
 
-    if (igName && address && paymentMethod && paymentDetails && treeSelection) {
-        let total = 0;
-        treeSelection.forEach(tree => {
-            let treeName = tree.split(' - ')[0];
-            total += treePrices[treeName];
-        });
-        text = `New Order:\nIG Name or Alias: ${igName}\nPhone Number: ${phoneNumber}\nAddress: ${address}\nMailbox Dropoff: ${mailboxDropoff ? 'Yes' : 'No'}\nTree(s) From the Menu: ${treeSelection.join(', ')}\nTotal Price: $${total}\nPayment Method: ${paymentMethod}\nPayment Details: ${paymentDetails}`;
+    let text = '';
+
+    if (igName && address && paymentMethod && paymentDetails && customRequest) {
+        text = `New Order:\nIG Name or Alias: ${igName}\nPhone Number: ${phoneNumber}\nAddress: ${address}\nMailbox Dropoff: ${mailboxDropoff ? 'Yes' : 'No'}\nCustom Request: ${customRequest}\nPayment Method: ${paymentMethod}\nPayment Details: ${paymentDetails}`;
         if (afterCutOff) {
             text += "\n\nNote: This order has been made after the cut-off time!";
         }
